44c3ef6a46f55cbdf422a1a2b9e95cf95b227433
Siddharth Verma <siddharthverma314@gmail.com>
2021-06-21 00:35:25 -0700

    [feat] Created flake repo

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          devShell = pkgs.haskell.packages.ghc8104.shellFor {
+            buildInputs = with pkgs; [
+              rnix-lsp
+              cabal-install
+              haskell-language-server
+            ];
+            packages = ps: with ps; [
+              vector
+            ];
+          };
+        }
+    );
+}
